1b2ac1f18742bc584c2fb3ddfed5814a83864f74
devsadiax <ftale8676@gmail.com>
2026-07-29 01:02:06 +0500

    Fix dbname assignment in config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $servername = getenv("MYSQLHOST") ?: "localhost";
 $username   = getenv("MYSQLUSER") ?: "root";
 $password   = getenv("MYSQLPASSWORD") ?: "";
-$dbname     = getenv("MYSQLDATABASE") ?: "pet_sphere";
+$dbname     = "pet_sphere";
 $dbport     = getenv("MYSQLPORT") ?: 3306;
 
 $conn = new mysqli($servername, $username, $password, $dbname, $dbport);
